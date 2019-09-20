1ffa8bd6c0765c4a9cb53a21c0a5f6ae8fba293d
maxiangfeng <411032443@qq.com>
2019-09-20 16:45:00 +0800

    CAS描述

--- nio/src/com/xfma/juc/TestCAS.java
+++ nio/src/com/xfma/juc/TestCAS.java
@@ -3,6 +3,21 @@ package com.xfma.juc;
 import java.util.Random;
 
 /**
+ * AtomicInteger 使用CAS算法
+ * CAS：compare-and-swap 比较和交换
+ * 核心思想就是：CAS中使用了三个数来保证原子性；
+ *      V:原来内存中的值
+ *      A:交换的时候进行比较，再次获取内存中的值
+ *      B:新值
+ *    在执行操作的时候，会拿 V == A 进行比较，如果返回true，则进行替换内存中的值
+ *    如果返回false，则进行新一轮的操作（失败重试），直到true为止
+ *
+ * 当然不使用AtomicInteger，在进行num++的时候使用同步锁，还是能解决问题，但是同步锁是悲观锁，比较重量级
+ * CAS是乐观锁，失败重试机制
+ *
+ */
+
+/**
  * 模拟实现CAS算法
  * CAS缺点：一直在失败重试，占用太多CPU资源，高并发的情况下，服务器压力比较大
  *http://www.imooc.com/article/details/id/44189
